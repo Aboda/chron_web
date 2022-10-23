5a88b3126c70bde577baa4e22bf2164feb810dd1
Demian Sanchez <andthenbeyond@gmail.com>
2022-10-23 14:21:39 -0500

    corrected body reference, removed base size

--- home.js
+++ home.js
@@ -1,9 +1,7 @@
 function build_big_image_container(){
-    body.append(ao.qq({
+    window.body.append(ao.qq({
         "nodetype":"img",
-        "src":"https://synchronicity.cloud/midlogo.png",
-        "width":"200px",
-        "height":"200px"
+        "src":"https://synchronicity.cloud/midlogo.png"
     }))
 }
 
