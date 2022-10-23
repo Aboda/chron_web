0da35b814b4ea2c886647345132a3f7cbaad4a69
Demian Sanchez <andthenbeyond@gmail.com>
2022-10-23 14:17:49 -0500

    added base art to home

--- home.js
+++ home.js
@@ -1,3 +1,11 @@
+function build_big_image_container(){
+    body.append(ao.qq({
+        "nodetype":"img",
+        "src":"https://synchronicity.cloud/midlogo.png"
+    }))
+}
+
 window.onload = () => {
     build_headbar()
+    build_big_image_container()
 }
