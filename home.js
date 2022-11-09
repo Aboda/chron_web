53842a50b68b7405c7af2b3bba20b31101985950
Demian Sanchez <andthenbeyond@gmail.com>
2022-11-09 11:44:07 -0600

    corrected color position

--- home.js
+++ home.js
@@ -27,17 +27,36 @@ function build_first_line(){
     })
     row.append(second_cell)
 
-    first_cell.append(build_compund_main_message())
+    first_cell.append(build_compound_main_message())
     second_cell.append(build_appearing_text_block("right appearing text"))
 
     return container
 }
 
+function build_compound_main_message(){
+    let composition_container = ao.qq({
+        "nodetype":"div",
+        "id":"main_comp_1"
+    })
+
+    let biggest_letters
+    let just_big_letters
+    let get_it_icon
+
+    return composition_container
+}
+
 function build_second_line(){
+
+    let table = ao.qq({
+        "nodetype":"table"
+    })
+
     let row = ao.qq({
         "nodetype":"tr",
         "styles":["row_height"]
     })
+    table.append(row)
 
     let first_cell = ao.qq({
         "nodetype":"td"
@@ -49,16 +68,28 @@ function build_second_line(){
     })
     row.append(second_cell)
 
-    first_cell.append(build_appearing_text_block("Second First"))
-    second_cell.append(build_appearing_text_block("Second Second"))
+    first_cell.append(build_appearing_text_block("Synchronicity helps you analize google calendars for available timeframes of the size of your choice, generating a simple shareable output:"))
+    second_cell.append(build_appearing_text_block(`Thu 11/10, 9a - 1p, 2p - 5p
+    Fri 11/11, 9a - 1p, 3p - 5p
+    Mon 11/14, 10a - 12p, 1p - 5p
+    Tue 11/15, 9a - 10a, 11a - 1p, 2p - 5p
+    Wed 11/16, 9a - 11a, 12p - 1p, 2p - 5p`))
 
-    return row
+    return table
 }
 
-function build_main_message(text){
+function build_appearing_title_block(text){
     return ao.qq({ 
         "nodetype":"p",
-        "styles":["soft_appear"],
+        "styles":["main_clear_text","soft_appear"],
+        "innerText":text
+    })
+}
+
+function build_appearing_subtitle_block(text){
+    return ao.qq({ 
+        "nodetype":"p",
+        "styles":["subtitle_clear_text","soft_appear"],
         "innerText":text
     })
 }
@@ -66,7 +97,7 @@ function build_main_message(text){
 function build_appearing_text_block(text){
     return ao.qq({ 
         "nodetype":"p",
-        "styles":["main_clear_text","soft_appear"],
+        "styles":["soft_appear"],
         "innerText":text
     })
 }
