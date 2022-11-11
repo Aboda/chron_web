2905d586310f6af5a35bf8e747394a46550b3eac
Demian Sanchez <andthenbeyond@gmail.com>
2022-11-10 20:27:15 -0600

    reordering content

--- home.js
+++ home.js
@@ -28,10 +28,13 @@ function build_first_line(){
     row.append(second_cell)
 
     first_cell.append(build_compound_main_message())
-    second_cell.append(ao.qq({
-        "nodetype":"img",
-        "src":"https://synchronicity.cloud/appscreenshot.png"
-    }))
+
+    second_cell.append(build_appearing_text_block(`Thu 11/10, 9a - 1p, 2p - 5p
+    Fri 11/11, 9a - 1p, 3p - 5p
+    Mon 11/14, 10a - 12p, 1p - 5p
+    Tue 11/15, 9a - 10a, 11a - 1p, 2p - 5p
+    Wed 11/16, 9a - 11a, 12p - 1p, 2p - 5p`))
+    
     return container
 }
 
@@ -76,15 +79,15 @@ function build_second_line(){
     })
     row.append(second_cell)
 
-    first_cell.append(build_appearing_text_block(`Synchronicity helps you analize your google calendars
+    first_cell.append(build_appearing_text_block(`Synchronicity helps you analyze your google calendars
     for available timeframes of the size of your choice,
     generating a simple shareable output:`))
 
-    second_cell.append(build_appearing_text_block(`Thu 11/10, 9a - 1p, 2p - 5p
-    Fri 11/11, 9a - 1p, 3p - 5p
-    Mon 11/14, 10a - 12p, 1p - 5p
-    Tue 11/15, 9a - 10a, 11a - 1p, 2p - 5p
-    Wed 11/16, 9a - 11a, 12p - 1p, 2p - 5p`))
+
+    second_cell.append(ao.qq({
+        "nodetype":"img",
+        "src":"https://synchronicity.cloud/appscreenshot.png"
+    }))
 
     return table
 }
