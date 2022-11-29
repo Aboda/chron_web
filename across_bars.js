936b0b51c097c74bd0a769ad94e4d6b3cf8baea2
Demian Sanchez <andthenbeyond@gmail.com>
2022-11-28 19:36:58 -0600

    color and size of svg

--- across_bars.js
+++ across_bars.js
@@ -98,7 +98,7 @@ function build_displayed_options(links){
 
 function build_hamburger_options(links){
     let burger_svg = `
-    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0z" fill="none"/><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/></svg>`
+    <svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 0 24 24" width="48px" fill="#000000"><path d="M0 0h24v24H0z" fill="white"/><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/></svg>`
 
     let burger_button = ao.qq({
         "nodetype":"div",
