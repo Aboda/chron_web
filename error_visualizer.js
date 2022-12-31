3f5533eb69e793ebf3698d2cfd585171e7558874
Demian Sanchez <andthenbeyond@gmail.com>
2022-12-31 11:09:40 -0600

    building

--- error_visualizer.js
+++ error_visualizer.js
@@ -48,14 +48,15 @@ function fill_data_displays() {
     let input_text = ao.simple.error_report_data_input.node.innerText
     let data_length = input_text.length
     let data = JSON.parse(input_text)
+    let mc = ao.simple.main_container.node
     console.log(data)
     ao.simple.ingest_button.node.disabled = true
     ao.simple.reset_button.node.disabled = false
-    ao.simple.main_container.node.append(build_datapacket_resume(data,data_length))
-    ao.simple.main_container.node.append(build_time_references_1(data))
-    ao.simple.main_container.node.append(build_essential_card("output",data.last_event_report.output))
-    ao.simple.main_container.node.append(build_time_references_2(data))
-
+    mc.append(build_datapacket_resume(data,data_length))
+    mc.append(build_time_references_1(data))
+    mc.append(build_essential_card("output",data.last_event_report.output))
+    mc.append(build_time_references_2(data))
+    mc.append(build_days_card(data))
 
     let exclusion_list = {
         "version":true,
@@ -65,7 +66,10 @@ function fill_data_displays() {
         "last_connection":true,
         "script_user_timezone":true,
         "script_user_offset":true,
-        "output":true
+        "output":true,
+        "duration":true,
+        "hour_frame":true,
+        "date_frame":true
     }
 
     for (let item in data.last_event_report){
@@ -114,88 +118,25 @@ function build_time_references_2 (data){
     return container
 }
 
-function assemble_settings_display(data){
-    let first_settings_container  = ao.qq({
-        "id":"first_settings_container",
-        "nodetype":"div",
-        "styles":["hor_flex","card25x14"]
-    })
 
+
+function build_days_card(data){
     let dotw_container = ao.qq({
         "id":"dotw_container",
         "nodetype":"div",
-        "styles":["vertical_list"]
+        "styles":["vertical_list","basic_display_card"]
     })
-    first_settings_container.append(dotw_container)
 
     for (let ordered_day of data.last_event_report.weekdays) {
         dotw_container.append(
             ao.qq({
                 "nodetype":"p",
-                "id":"settings_dotw_"+ordered_day,
                 "innerText":ordered_day + " "+rewrite_boolean(data.last_event_report.dotw[ordered_day],"Consider","Ignore")
             })
         )
-        
     }
 
-    let main_query_settings_container = dotw_container = ao.qq({
-        "id":"main_query_settings_container",
-        "nodetype":"div",
-        "styles":["vertical_list"]
-    })
-    first_settings_container.append(main_query_settings_container)
-
-    let duration_display = ao.qq({
-        "nodetype":"div",
-        "id":"duration_display",
-        "styles":["hor_flex"],
-        "innerText":data.last_event_report.duration
-    })
-    main_query_settings_container.append(duration_display)
-
-    let timeframe_display = ao.qq({
-        "nodetype":"div",
-        "id":"timeframe_display",
-        "styles":["hor_flex"],
-        "innerText":data.last_event_report.hour_frame.start.hours + ":"+data.last_event_report.hour_frame.start.minutes + " to " + data.last_event_report.hour_frame.end.hours + ":"+data.last_event_report.hour_frame.end.minutes
-    })
-    main_query_settings_container.append(timeframe_display)
-
-    let user_script_utc_timezone = ao.qq({
-        "nodetype":"div",
-        "id":"user_script_utc_timezone",
-        "styles":["hor_flex"],
-        "innerText":data.last_event_report.script_user_timezone + " " + data.last_event_report.script_user_offset
-    })
-    main_query_settings_container.append(user_script_utc_timezone)
-    
-    let date_frame_dsiplay = ao.qq({
-        "nodetype":"div",
-        "id":"date_frame_dsiplay",
-        "styles":["hor_flex"],
-        "innerText":data.last_event_report.date_frame.start + " to " + data.last_event_report.date_frame.end
-    })
-    main_query_settings_container.append(date_frame_dsiplay)
-
-    let target_offset_display = ao.qq({
-        "nodetype":"div",
-        "id":"target_offset_display",
-        "styles":["hor_flex"],
-        "innerText":data.last_event_report.target_offset
-    })
-    main_query_settings_container.append(target_offset_display)
-
-    return first_settings_container
-}
-
-function assemble_events_display(data){
-    let placeholder = ao.qq({
-        "nodetype":"div",
-        "innerText":JSON.stringify(data.last_event_report.events_from_blocking_calendars,null,2,true)
-    })
-
-    return placeholder
+    return
 }
 
 function rewrite_boolean(boolean,value_if_true,value_if_else){
@@ -204,36 +145,6 @@ function rewrite_boolean(boolean,value_if_true,value_if_else){
     }else{
         return value_if_else
     }
-}
-
-function assemble_settings_change(){
-    ao.simple.main_container.node.append(
-        ao.qq({
-            "id":"settings_change",
-            "nodetype":"div",
-            "styles":["vertical_list","card25x14"]
-        })
-    )
-}
-
-function assemble_events_pool(){
-    ao.simple.main_container.node.append(
-        ao.qq({
-            "id":"events_list",
-            "nodetype":"div",
-            "styles":["vertical_list"]
-        })
-    )
-}
-
-function assemble_availability_map(){
-    ao.simple.main_container.node.append(
-        ao.qq({
-            "id":"availability_map",
-            "nodetype":"div",
-            "styles":["vertical_list"]
-        })
-    )
 }
 
 function build_essential_card(title,content){
